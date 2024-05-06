9bfc4cd5b886fd1431fc3990e2f0e9a241d03789
Hassan8725 <hassanahmed8725@gmail.com>
2024-05-06 21:52:25 +0200

    nox file

--- noxfile.py
+++ noxfile.py
@@ -2,14 +2,12 @@
 import nox
 from nox.sessions import Session
 
-
 @nox.session(venv_backend="none")
 def test(session: Session) -> None:
     """Run tests with pytest and create coverage report."""
-    session.run("sync", external=True)
-    #session.run("sync", "-G", "dev", external=True)
+    session.run("sync", external=True)  # Correctly calling sync without arguments
     session.run(
         "run",
         "pytest",
         *session.posargs,
-    )
+        )
