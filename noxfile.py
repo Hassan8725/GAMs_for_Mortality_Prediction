0bb52b4adbbeedd1c52d256aff0e38aecdcd1a47
Hassan8725 <hassanahmed8725@gmail.com>
2024-05-06 21:50:24 +0200

    nox file updated

--- noxfile.py
+++ noxfile.py
@@ -6,7 +6,8 @@ from nox.sessions import Session
 @nox.session(venv_backend="none")
 def test(session: Session) -> None:
     """Run tests with pytest and create coverage report."""
-    session.run("sync", "-G", "dev", external=True)
+    session.run("sync", external=True)
+    #session.run("sync", "-G", "dev", external=True)
     session.run(
         "run",
         "pytest",
