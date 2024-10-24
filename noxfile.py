493113003687e5d8e7320fcaa6b23ac290ca0cc0
Hassan8725 <hassanahmed8725@gmail.com>
2024-10-24 06:15:43 +0200

    noxfile updated

--- noxfile.py
+++ noxfile.py
@@ -3,10 +3,13 @@ import nox
 from nox.sessions import Session
 
 
-@nox.session(venv_backend="none")
+@nox.session(venv_backend="virtualenv")
 def test(session: Session) -> None:
     """Run tests with pytest and create coverage report."""
-    session.run("sync", external=True)
+    # Install dependencies from requirements.txt
+    session.install("-r", "requirements.txt")
+    
+    # Run pytest
     session.run(
         "pytest",
         *session.posargs,
